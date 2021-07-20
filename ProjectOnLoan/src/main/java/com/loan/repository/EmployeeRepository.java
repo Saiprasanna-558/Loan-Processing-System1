33d5dc9959f66e81abcf94c9ae89c7eda98bec02
Saiprasanna-558 <84898952+Saiprasanna-558@users.noreply.github.com>
2021-07-20 09:43:01 +0530

    Add files via upload

---
+++ ProjectOnLoan/src/main/java/com/loan/repository/EmployeeRepository.java
@@ -0,0 +1,21 @@
+package com.loan.repository;
+
+
+
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.loan.entity.Employee;
+
+@Repository
+@Transactional
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+
+	
+
+}
